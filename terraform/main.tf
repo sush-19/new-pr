5fff86c66f9b34bb2f9cad0bab106a1df5c8d65a
sush-19 <114879514+sush-19@users.noreply.github.com>
2023-01-27 18:23:19 +0000

    updated

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,11 +4,11 @@ access_key = "AKIAZLUXQ5K3VSZ64BUC"
 secret_key = "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"
 }
 
-resource "aws_instance" "one"{
+resource "aws_instance" "dev_server"{
 ami = "ami-0aa7d40eeae50c9a9"
 instance_type = "t2.micro"
 key_name = "mono"
-security_groups = ["jenkins-setup"]
+vpc_security_group_ids = [aws_security_group.jenkins-setup.id]
 tags = {
 Name = "my-instance"
 Environment = "dev"
