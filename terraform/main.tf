a34f16407f547b427036859a918706a53d0283c4
sush-19 <114879514+sush-19@users.noreply.github.com>
2023-01-27 18:32:28 +0000

    save

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,12 +4,11 @@ access_key = "AKIAZLUXQ5K3VSZ64BUC"
 secret_key = "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"
 }
 
-resource "aws_instance" "key"{
+resource "aws_instance" "key" {
 ami = "ami-0aa7d40eeae50c9a9"
 instance_type = "t2.micro"
-key_name = "mono"
 vpc_security_group_ids = [aws_security_group.demo-sg.id]
 tags = {
-Name = "my-instance"
+Name = "auto-instance"
 }
 }
